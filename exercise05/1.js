174b46297f2d833635876cc2aba5fdf7f66f7a0d
autyauth <thanakornrattanapornchai@gmail.com>
2023-02-22 19:02:28 +0700

    exercise 4 move a block of code

---
+++ exercise05/1.js
@@ -0,0 +1,12 @@
+function startWorker() {
+    if(typeof(Worker) !== "undefined") {
+      if(typeof(w) == "undefined") {
+        w = new Worker("demo_workers.js");
+      }
+      w.onmessage = function(event) {
+        document.getElementById("result").innerHTML = event.data;
+      };
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
+    }
+  }
